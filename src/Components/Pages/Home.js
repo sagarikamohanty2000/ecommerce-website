ad6cf2c66d74017fac136a1d80f60f6ec5b6d823
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2024-03-04 20:37:06 +0530

    homePage/navigation

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,7 +1,20 @@
+import React from "react";
+import HomePageTour from "./HomePageTour";
+import classes from "./Home.module.css";
+
 const Home = () => {
-    return(
-        <h1>Home</h1>
-    )
-}
+  return (
+    <>
+      {" "}
+      <div className={classes["header-second"]}>
+        <h1>The Generics</h1>
+
+        <button className={classes.albumBtn}> Get out latest Album</button>
+        <button className={classes.playBtn}>▶</button>
+      </div>
+      <HomePageTour />
+    </>
+  );
+};
 
 export default Home;
